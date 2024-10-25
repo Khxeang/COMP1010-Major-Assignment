5483d824d0a5a9e5257faecfe4b8d07fa3e27d46
Khxeang <mengkheang.ly@students.mq.edu.au>
2024-10-25 16:05:55 +1100

    Created Character.java
    Finished building character logic and code.

--- src/Character.java
+++ src/Character.java
@@ -1,29 +1,57 @@
 public class Character {
-    String name;
-    int health;
-    int strength;
-    int defense;
+    String name; //Name of the character.
+    int health; //Character's health points.
+    int defense; //Character's defense points.
+    int passiveMinDamage; //Character's minimum passive damage. 
+    int passiveMaxDamage; //Character's maximum passive damage.
+    int itemDamage; //Damage dealt by character's items.
+    int UltimateDamage; //Damage dealt by character's ultimate attacks.
 
-    // Constructor
-    public Character(String name, int health, int strength, int defense) {
+
+    // Constructor for character initialization
+    public Character(String name, int health, int defense, int passiveMinDamage, int passiveMaxDamage, int itemDamage, int UltimateDamage) {
         this.name = name;
         this.health = health;
-        this.strength = strength;
         this.defense = defense;
+        this.passiveMinDamage = passiveMinDamage;
+        this.passiveMaxDamage = passiveMaxDamage;
+        this.itemDamage = itemDamage;
+        this.UltimateDamage = UltimateDamage;
     }
 
     // Method to attack another character
     public void attack(Character target) {
-        int damage = (this.strength - target.defense) + (int) (Math.random() * 6);
-        if (damage > 0) {
-            target.health -= damage;
-            System.out.println(this.name + " attacked " + target.name + " and dealt " + damage + " damage!");
-        } else {
-            System.out.println(target.name + " blocked the attack!");
-        }
+        int damage = DetermineDamage(); //Call a function that calculate the attack damage.
+        target.TakeDamage(damage); //Inflict damage upon the target.
+        System.out.println(this.name + "attacked" + target.getName() + "and dealt" + damage + "damage!"); //Make announcement of the attack.
     }
 
+    //Function that determine the damage based on character's passive attack range.
+    private int DetermineDamage(){
+        return passiveMinDamage + (int)(Math.random()*(passiveMaxDamage - passiveMinDamage)); //Randomizng the output damage of every character between min and max.
+    }
+
+    //Function for inflicting damage and reducing character's health
+    public void TakeDamage(int damage){
+        this.health -= Math.max(damage - this.defense, 0); //Will inflict damage onto characters reducing their defense points first.
+    }
+
+    //Function to check if the character is alive or not.
     public boolean isAlive() {
         return this.health > 0;
+    }
+
+    //Functions to retrieve the name, health, and the defense points of the character.
+    public String getName(){
+        return name;
+    }
+
+
+    public int getHealth(){
+        return health;
+    }
+
+    public int getDefense(){
+        return defense;
     }
 }
