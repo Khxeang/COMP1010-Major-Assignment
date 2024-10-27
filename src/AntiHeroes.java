ae63401586719800c9a4cce13f4a04b2071fcaad
Iain2005 <Iain.lolohea@yahoo.com>
2024-10-27 18:26:29 +1100

    MCU arenas Version 2.0
    just expanding the constructor to add item and ultimate name

--- src/AntiHeroes.java
+++ src/AntiHeroes.java
@@ -1,6 +1,6 @@
 public class AntiHeroes extends Character{ //Anti-Hero class is inheriting from the Character class.
-    public AntiHeroes(String name, int health, int defense, int passiveMinDamage, int passiveMaxDamage, int itemDamage, int UltimateDamage){
-        super(name, health, defense, passiveMinDamage, passiveMaxDamage, itemDamage, UltimateDamage); //Calls the Character constructor to set name, health, defense, passiveMinDamage, passiveMaxDamage, itemDamage, and UltimateDamage.
+    public AntiHeroes(String name, int health, int defense, int passiveMinDamage, int passiveMaxDamage, int itemDamage, int UltimateDamage, String ultimateAttackName ,Item Item){
+        super(name, health, defense, passiveMinDamage, passiveMaxDamage, itemDamage, UltimateDamage, ultimateAttackName ,Item); //Calls the Character constructor to set name, health, defense, passiveMinDamage, passiveMaxDamage, itemDamage, and UltimateDamage.
     }
 
     public String getType(){
--- src/Villains.java
+++ src/Villains.java
@@ -1,6 +1,6 @@
 public class Villains extends Character { //Villian class is inheriting from the Character class.
-    public Villains(String name, int health, int defense, int passiveMinDamage, int passiveMaxDamage, int itemDamage, int UltimateDamage){ 
-        super(name, health, defense, passiveMinDamage, passiveMaxDamage, itemDamage, UltimateDamage); //Calls the Character constructor to set name, health, defense, passiveMinDamage, passiveMaxDamage, itemDamage, and UltimateDamage.
+    public Villains(String name, int health, int defense, int passiveMinDamage, int passiveMaxDamage, int itemDamage, int UltimateDamage, String ultimateAttackName, Item Item){ 
+        super(name, health, defense, passiveMinDamage, passiveMaxDamage, itemDamage, UltimateDamage, ultimateAttackName ,Item); //Calls the Character constructor to set name, health, defense, passiveMinDamage, passiveMaxDamage, itemDamage, and UltimateDamage.
     }
     
     public String getType(){
